666c73ffcf37cdc11a421a1f2cc7992098a3e315
Zubenna <emelunna@gmail.com>
2021-03-06 08:38:42 +0100

    Updating Project Files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing React Calculator Project Setup. Testing ..Testing</h1>
+      <h1>Testing React Calculator Project Setup. Testing</h1>
     </div>
   );
 }
