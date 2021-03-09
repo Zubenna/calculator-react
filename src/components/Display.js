6d199ba7f485899b0480b580220d5f5b4c533bde
Zubenna <emelunna@gmail.com>
2021-03-09 07:12:14 +0100

    Using Single Quotes for Strings

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Display = (props) => (
-  <input className="display" type="text" value={props.result}></input>
+const Display = props => (
+  <input className="display" type="text" value={props.result} />
 );
 
 export default Display;
