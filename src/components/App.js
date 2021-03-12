e6404288ef27bb1c00844585b99bc996ab79a574
Zubenna <emelunna@gmail.com>
2021-03-12 05:55:55 +0100

    Adding Handleclick Function to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,45 @@ import React from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// import calculate from '../logic/calculate';
+import React, { Component } from 'react'
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Adding Basic Components: Displaying Calculator UI Version 1</h1>
-      <section className="calc-container">
-        <>
-          <Display result="0" />
-          <ButtonPanel />
-        </>
-      </section>
-    </div>
-  );
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      total: null,
+      operation: null,
+      next: null
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (buttonName) => {
+    const changedState = calculate(this.state, buttonName);
+    this.setState(changedState);
+  }
+
+  showResult = state => {
+    const { total, next, operation } = state;
+    const result = `${total}${operation}${next}`.replace(/null/g, '');
+    return result === '' ? undefined : result;
+  };
+
+  render() {
+    return (
+      <div className="App">
+       <h1>Simple Calculator UI Version 1</h1>
+       <section className="calc-container">
+         <>
+           <Display result={showResult(this.state)} />
+           <ButtonPanel onClick={this.handleClick}/>
+         </>
+       </section>
+     </div>
+    )
+  }
 }
 
 export default App;
+
